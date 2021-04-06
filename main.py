d2dc7d49ef900d410698d5a97e34251580e6c7b0
9hax <paule.pauling@gmail.com>
2021-04-06 22:00:21 +0200

    make app compatible with docker

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-# main files for a simple open source ticketing system
-# written from scratch by 9hax
-# this uses the flask framework
-
-from flask import *
-from config import *
-
-app = Flask(__name__)
-
-@app.route('/')
-def printTickets():
-    return render_template('index.html', sitename=SITE_NAME, username="Textuser")
-
-app.run(debug = True)
+from simpleticket import app

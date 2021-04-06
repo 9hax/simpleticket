9ac9ed785bdbe42474934ea437f0280f4c43f427
Paul Friedrich Vierkorn <pfvierkorn@gmail.com>
2021-04-06 11:06:01 +0200

    inital commit

---
+++ config.py
@@ -0,0 +1,16 @@
+# interface ip and port number to launch the ticket system on.
+# setting 0.0.0.0 as the interface ip exposes the flask host on all available interfaces.
+INTERFACE_IP = "0.0.0.0"
+INTERFACE_PORT = "80"
+
+#require login for reading and writing to tickets and to create new tickets?
+REQUIRE_LOGIN = True
+
+#the password salt used to salt the hashes for the database.
+# warning! changing the password salt will make all passwords in the system invalid.
+PASSWORD_SALT = "randomletters" 
+
+#language that the web interface for simpleticket uses. en_EN is default.
+LANGUAGE = "de_DE"
+#the language system passes this string if it cannot find the default string for the specified language.
+DEFAULT_STRING = "There should a a string here, but we couldn't read it from the language file database. Please report this to the devs at https://github.com/9hax/simpleticket"
---
+++ main.py
@@ -0,0 +1,6 @@
+#!/bin/env python3
+
+# main files for a simple open source ticketing system
+# written from scratch by 9hax
+# this uses flask framework
+

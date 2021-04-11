7a61151263e7b912a94c2333a6bf93648c9b1f3f
9hax <paule.pauling@gmail.com>
2021-04-11 21:27:33 +0200

    add a function for sending mail

--- user.py
+++ user.py
@@ -1,8 +1,15 @@
 from simpleticket import m
 import bcrypt
+import smtpconfig
+import json
 
-def resetpw(kek):
-    print(kek)
+# prepare language files
+
+with open("lang/"+config.LANGUAGE+".json",'r',encoding="utf-8") as langfile:
+    lang = json.load(langfile)
+
+def resetpw(email):
+    sendmail(email, "Place an email text here.")
 
 def verify_login(u, p):
     potential_user = m.User.query.filter_by(username=u).first()
@@ -27,3 +34,10 @@ def create_user(username, email, hashedPassword, passwordResetTimer = -1, highPe
     new_user.highPermissionLevel = highPermissionLevel
     m.db.session.add(new_user)
     m.db.session.commit()
+
+def sendmail(address, htmlcontent):
+    import smtplib, ssl
+    sslContext = ssl.create_default_context()
+    with smtplib.SMTP_SSL(config.SMTP_SERVER, config.SMTP_PORT, context=sslContext) as smtpserver:
+        smtpserver.login(config.SMTP_USER, config.SMTP_PASSWORD)
+        smtpserver.sendmail(config.SMTP_USER, address, htmlcontent)

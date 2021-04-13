b86730940398b6ce9f728964a686d04c5005b03d
Paul Friedrich Vierkorn <pfvierkorn@gmail.com>
2021-04-13 23:46:55 +0200

    redo some naming

--- user.py
+++ user.py
@@ -1,8 +1,8 @@
-from simpleticket import m
 import bcrypt
 import config
 import smtpconfig
 import json
+from simpleticket import m
 
 # prepare language files
 
@@ -23,8 +23,8 @@ def verify_login(u, p):
 def hashPassword(password):
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt(12)).decode()
 
-def get_user(id):
-    return m.User.query.get(id)
+def get_user(userid):
+    return m.User.query.get(userid)
 
 def create_ticket(title, text, media, created_by, assigned_to):
     new_ticket = m.Ticket()
@@ -36,13 +36,6 @@ def create_ticket(title, text, media, created_by, assigned_to):
     new_ticket.assigned_to = assigned_to
     m.db.session.add(new_ticket)
     m.db.session.commit()
-
-def edit_ticket(current_ticket_id, new_open, new_assignee):
-    current_ticket = m.Ticket.query.filter_by(id = current_ticket_id)
-    current_ticket.is_open = new_open
-    current_ticket.assigned_to = new_assignee
-    m.db.session.commit()
-
 
 def create_user(username, email, hashedPassword, passwordResetTimer = -1, highPermissionLevel = 0):
     new_user = m.User()
@@ -56,7 +49,7 @@ def create_user(username, email, hashedPassword, passwordResetTimer = -1, highPe
 
 def sendmail(address, htmlcontent):
     import smtplib, ssl
-    sslContext = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtpconfig.SMTP_SERVER, smtpconfig.SMTP_PORT, context=sslContext) as smtpserver:
+    ssl_context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtpconfig.SMTP_SERVER, smtpconfig.SMTP_PORT, context=ssl_context) as smtpserver:
         smtpserver.login(smtpconfig.SMTP_USER, smtpconfig.SMTP_PASSWORD)
         smtpserver.sendmail(smtpconfig.SMTP_USER, address, htmlcontent)

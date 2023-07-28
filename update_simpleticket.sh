b19249eb2869e0c6b720507422fe79a4333ade18
9hax <me@9hax.net>
2023-07-28 09:04:43 +0200

    fix update script for the correct permissions

--- update_simpleticket.sh
+++ update_simpleticket.sh
@@ -15,16 +15,15 @@ cp /opt/simpleticket/* /opt/simpleticket_backups/$(date +"%d-%m-%Y") -R
 # Pull changes
 echo ${green}Pulling updates...${reset}
 cd /opt/simpleticket
-sudo git pull
+sudo -u simpleticket git pull
 
-# make simpleticket directory accessible
-echo ${green}Changing directory perms...${reset}
-sudo chmod 777 /opt/simpleticket -R
 
 # Start database migrations
 echo ${green}Running database migrations...${reset}
-flask db upgrade
-flask db migrate -m "SimpleTicket Update"
+sudo -u simpleticket py -m flask db upgrade
+sudo -u simpleticket py -m flask db migrate -m "SimpleTicket Update"
+sudo -u simpleticket py -m flask db upgrade
+sudo -u simpleticket py -m flask db migrate -m "SimpleTicket Update"
 
 # Start apache2
 echo ${green}Starting apache...${reset}
